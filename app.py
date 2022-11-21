e491773033a758d38ed17201c97d7d7ca5650df8
MicGuest <118722862+MicGuest@users.noreply.github.com>
2022-11-21 17:14:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ model = pipeline(
 )
 
 def predict(prompt):
-    summary = model(prompt)[0]["summary_text"]
+    #summary = model(prompt)[0]["summary_text"]
+    summarizer = pipeline(“summarization”, model=”t5-base”, tokenizer=”t5-base”, framework=”tf”)
+    summary=summarizer(context, max_length=130, min_length=60)
     return summary
 
 
